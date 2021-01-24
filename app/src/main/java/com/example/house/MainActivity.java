ed5d40128a25fae313cfaa78c9af899787cbedb3
GuK1337 <sb05032002@gmail.com>
2021-01-24 23:53:45 +0400

    first commit

---
+++ app/src/main/java/com/example/house/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.house;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private View toolBox;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolBox = findViewById(R.id.toolbox);
+    }
+
+    public void showSettings(View v){
+        if (toolBox.getVisibility() == View.GONE)
+            toolBox.setVisibility(View.VISIBLE);
+        else
+            toolBox.setVisibility(View.GONE);
+    }
+
+}
